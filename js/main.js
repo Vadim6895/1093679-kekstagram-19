022cf34f13b1bf1db75000e41d940964d0870e02
Vadim6895 <54281107+Vadim6895@users.noreply.github.com>
2020-02-09 20:48:16 +0300

    module3-task2

--- js/main.js
+++ js/main.js
@@ -5,14 +5,22 @@ var MESSAGES = ['Всё отлично!', 'В целом всё неплохо. 
   'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
   'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
   'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
+var DESCRIPTION = ['Первое описание', 'Второе описание', 'Третье описание', 'Четвертое описание', 'Пятое описание'];
+
 var NUMBER_CARDS = 25;
 var MINLIKES = 15;
 var MAXLIKES = 200;
 
 var cards = [];
 
-var pictureContainer = document.querySelector('.pictures');
-var pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
+var PICTUREID = '#picture';
+var PICTURESCLASS = '.pictures';
+var PICTURE__COMMENTS = '.picture__comments';
+var PICTURE__IMG = '.picture__img';
+var PICTURE__LIKES = '.picture__likes';
+
+var pictureContainer = document.querySelector(PICTURESCLASS);
+var pictureTemplate = document.querySelector(PICTUREID).content;
 
 function randomInt(minInt, maxInt) {
   var min = Math.ceil(minInt);
@@ -20,8 +28,8 @@ function randomInt(minInt, maxInt) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
-function arrayRandElement(arr) {
-  var rand = Math.floor(Math.random() * arr.length);
+function getArrayRandElement(arr) {
+  var rand = Math.floor((randomInt(MINLIKES, MAXLIKES) / 200) * arr.length);
   return arr[rand];
 }
 
@@ -29,11 +37,9 @@ var generationCard = function () {
   var card = {
     avatar: 'img/avatar-' + randomInt(1, 6) + '.svg',
     likes: randomInt(MINLIKES, MAXLIKES),
-    comments: arrayRandElement(MESSAGES),
+    comments: getArrayRandElement(MESSAGES),
+    description: getArrayRandElement(DESCRIPTION)
   };
-  /* 1) Абсолюнто не ясно, где брать описание фото, поэтому его нет*/
-  /* 2) И каким образом адреса картинок не должны повторяться если нужно 25 обьектов а картинок 6 ?*/
-  /* Возможно 2ая часть задания прояснит ситуацию, пулл забирал*/
   return card;
 };
 
@@ -42,18 +48,22 @@ for (var i = 0; i < NUMBER_CARDS; i++) {
   cards.push(generationCard());
 }
 
-var renderCard = function (object) {
+var renderCard = function (card) {
   var pictureNode = pictureTemplate.cloneNode(true);
 
-  pictureNode.querySelector('.picture__comments').textContent = object.comments;
-  pictureNode.querySelector('.picture__img').src = object.avatar;
-  pictureNode.querySelector('.picture__likes').textContent = object.likes;
+  pictureNode.querySelector(PICTURE__COMMENTS).textContent = card.comments;
+  pictureNode.querySelector(PICTURE__IMG).src = card.avatar;
+  pictureNode.querySelector(PICTURE__LIKES).textContent = card.likes;
+
   return pictureNode;
 };
 
+var generateFragment = function () {
+  var fragment = document.createDocumentFragment();
+  for (var index = 0; index < cards.length; index++) {
+    fragment.appendChild(renderCard(cards[index]));
+  }
 
-var fragment = document.createDocumentFragment();
-for (var a = 0; a < cards.length; a++) {
-  fragment.appendChild(renderCard(cards[a]));
-}
-pictureContainer.appendChild(fragment);
+  pictureContainer.appendChild(fragment);
+};
+generateFragment();
